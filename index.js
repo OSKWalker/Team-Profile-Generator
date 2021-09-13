a3031b66026d6f6d830e83b5ddd663f24cca50be
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-09-12 23:46:38 -0400

    added first inquirer prompts. saving progress

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 import Manager from "./lib/Manager";
 import Engineer from "./lib/Engineer";
 import Intern from "./lib/Intern";
-import inquirer from "inquirer";
-import path from "path";
-import fs from "fs";
+import { prompt } from "inquirer";
+import { resolve, join } from "path";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+import render from "./src/template-helper.js";
 
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "my-team.html");
+const roster = [];
+const memberIDs = [];
 
-import render from "./src/template-helper.js";
+function init(){
+    console.log("Team Profile Generator Initialized");
+
+    inquirer.prompt([
+        {
+            name: "teamManagerName",
+            type: "input",
+            message: "Who is your Team Manager?",
+            validate: answer =>{
+            if (answer !== ""){
+                return true;
+            }
+            return "Please enter one or more characters."
+            }
+        },
+        {
+            name: "managerID",
+            type: "input",
+            message: "Enter your Team Manager's identification number.",
+            validate: answer => {
+                const valid = answer.match(/^[1-9]\d*$/);
+                
+                if (valid){
+                    return true;
+                }
+                return "Identification number must be greater than zero (0)."
+            }
+        },
+        {
+            
+        }
+    }
+    ])
+}
+
 console.log(render());
