cee10eacba452708bc57d372ff677b485e336feb
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-09-13 00:25:55 -0400

    saving progress

--- index.js
+++ index.js
@@ -11,39 +11,79 @@ const outputPath = path.join(OUTPUT_DIR, "my-team.html");
 const roster = [];
 const memberIDs = [];
 
-function init(){
-    console.log("Team Profile Generator Initialized");
+function init() {
+  console.log("Team Profile Generator Initialized");
 
-    inquirer.prompt([
+  function setManager() {
+    console.log("No team is complete without someone to manage it.\n");
+    inquirer
+      .prompt([
         {
-            name: "teamManagerName",
-            type: "input",
-            message: "Who is your Team Manager?",
-            validate: answer =>{
-            if (answer !== ""){
-                return true;
+          name: "teamManagerName",
+          type: "input",
+          message: "Enter your Team Manager's name:",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
             }
-            return "Please enter one or more characters."
-            }
+            return "Enter one or more characters.";
+          },
         },
         {
-            name: "managerID",
-            type: "input",
-            message: "Enter your Team Manager's identification number.",
-            validate: answer => {
-                const valid = answer.match(/^[1-9]\d*$/);
-                
-                if (valid){
-                    return true;
-                }
-                return "Identification number must be greater than zero (0)."
+          name: "managerID",
+          type: "input",
+          message: "Enter your Team Manager's identification number:",
+          validate: (answer) => {
+            const valid = answer.match(/^[1-9]\d*$/);
+
+            if (valid) {
+              return true;
             }
+            return "Identification number must be greater than zero (0).";
+          },
         },
         {
-            
-        }
-    }
-    ])
+          name: "managerEmail",
+          type: "input",
+          message: "Enter your Team Manager's E-mail address:",
+          validate: (answer) => {
+            const valid = answer.match(
+              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+            );
+
+            if (valid) {
+              return true;
+            }
+            return "Enter a valid e-mail address.";
+          },
+        },
+        {
+          name: "managerOfficeNumber",
+          type: "input",
+          message: "Enter your Team Manager's Office number:",
+          validate: (answer) => {
+            const valid = answer.match(/^[1-9]\d*$/);
+
+            if (valid) {
+              return true;
+            }
+            return "Office number must be greater than zero (0)";
+          },
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerID,
+          answers.managerEmail,
+          answers.managerOfficeNumber
+        );
+
+        roster.push(manager);
+
+        memberIDs.push(answers.managerID);
+
+        fillRoster();
+      });
+  }
 }
-
-console.log(render());
