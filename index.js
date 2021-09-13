395de8bdf83d6c48dbfe1d2f6e215d3633a90f1f
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-09-12 21:21:42 -0400

    saving progress

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ import Intern from "./lib/Intern";
 import inquirer from "inquirer";
 import path from "path";
 import fs from "fs";
+
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(OUTPUT_DIR, "my-team.html");
 import render from "./src/template-helper.js";
 console.log(render());
