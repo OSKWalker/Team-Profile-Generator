21c42a67b4f6e2ab1d1b0681ed368d54a2919226
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-09-15 23:35:11 -0400

    intern test buildout

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -5,4 +5,22 @@ describe("Intern", () => {
     const intern = new Intern();
     expect(typeof intern).toBe("object");
   });
+
+  test("Can set the intern school name via constructor arguments", () => {
+    const conArg = "School";
+    const intern = new Intern("Name", 123, "example@arguments.com", conArg);
+    expect(intern.school).toBe(conArg);
+  });
+
+  test("Can get the intern school name via constructor arguments", () => {
+    const conArg = "GitHub";
+    const intern = new Intern("Name", 123, "example@arguments.com", conArg);
+    expect(intern.getSchool()).toBe(conArg);
+  });
+
+  test('getRole() method should return "Intern"', () => {
+    const conArg = "Intern";
+    const intern = new Intern("Name", 123, "example@arguments.com", "School");
+    expect(intern.getRole()).toBe(conArg);
+  });
 });
